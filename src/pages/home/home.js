9d92042aadc9c7d516ebdd39db1e712d42ab155f
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-02-26 00:09:21 -0300

    Instala React Navigation

---
+++ src/pages/home/home.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+export default function HomeScreen() {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+}
