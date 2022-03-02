7842cc7ae1844d321b28ddd0afba3c1298205537
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-03-02 13:38:23 -0300

    Prepara base para API dos correios

--- views/pages/rastreio/styles.js
+++ views/pages/rastreio/styles.js
@@ -3,6 +3,8 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff0db",
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
   },
