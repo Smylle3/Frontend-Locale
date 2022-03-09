1c3623751ca67ec7aa76abaf7caabad4e9e2fccb
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-03-08 21:21:49 -0300

    Cria e estiliza a página de cadastro de encomendas

--- views/pages/areaRestrita/cadastro/index.js
+++ views/pages/areaRestrita/cadastro/index.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import styles from "./styles";
+import Gstyles from "../../../../Globalstyles";
 import NavBar from "../../../components/navbar";
 import config from "../../../../app.json";
+import ModalMy from "../../../components/modal";
+import InputMy from "../../../components/input";
 
 export default function Cadastro({ navigation }) {
-  const adress = config.origin;
-  const [code, setCode] = useState(null);
+  const [msgPass, setMsgPass] = useState("");
+  const [isModal, setIsModal] = useState(false);
+
+  const [code, setCode] = useState("");
   const [user, setUser] = useState(null);
   const [product, setProduct] = useState(null);
-  const [response, setResponse] = useState(null);
+  const [local, setLocal] = useState(config.origin);
 
   useEffect(() => {
     randomCode();
@@ -19,7 +28,7 @@ export default function Cadastro({ navigation }) {
 
   async function randomCode() {
     let result = "";
-    let length = 10;
+    let length = 20;
     let chars =
       "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     for (let i = length; i > 0; --i)
@@ -37,43 +46,67 @@ export default function Cadastro({ navigation }) {
   }, []);
 
   async function sendForm() {
-    let response = await fetch(`${config.urlRoot}create-tracking`, {
+    let response = await fetch(`${config.urlRoot}api/tracks/new-track`, {
       method: "POST",
-      body: JSON.stringify({
-        userId: user,
-        code: code,
-        product: product,
-        local: adress,
-      }),
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
+      body: JSON.stringify({
+        UserId: user,
+        code: code,
+        product: product,
+        local: local,
+      }),
     });
-
+    let resTackId = await response.json();
+    let resProduct = await fetch(`${config.urlRoot}api/products/new-product`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        TrackId: resTackId,
+        product: product,
+      }),
+    });
+    let jsonProduct = await resProduct.json();
+    if (jsonProduct == "Sucesso") {
+      setMsgPass("Encomenda cadastrada com sucesso!");
+      setIsModal(true);
+    } else if (jsonProduct == "Falha") {
+      setMsgPass("Falha ao cadastrar encomenda!");
+      setIsModal(true);
+    }
   }
 
   return (
-    <View style={[styles.container, styles.top]}>
+    <View style={Gstyles.scroollContainer}>
       <NavBar title="Cadastrar encomenda" navigation={navigation} />
-      <Text>
-        {adress} {user} {product} {code}
-      </Text>
-
-      <View style={styles.changePassForm}>
-        <Text style={styles.initTxt}>Cadastre uma encomenda</Text>
-        <View style={styles.inputsView}>
-          <TextInput
-            style={styles.inputPass}
-            placeholder="Nome do produto"
-            value={product}
-            onChangeText={(text) => setProduct(text)}
-          />
+      <ModalMy msgPass={msgPass} isModal={isModal} setIsModal={setIsModal} />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "position"}
+        style={{ marginTop: 15 }}
+      >
+        <View style={Gstyles.keyView}>
+          <View style={Gstyles.viewForm}>
+            <Text style={Gstyles.initTxt}>Cadastre uma encomenda</Text>
+            <InputMy
+              nameUser={product}
+              setNameUser={setProduct}
+              title="Nome do produto"
+              isPassword="no"
+            />
+            <TouchableOpacity
+              onPress={() => sendForm()}
+              style={Gstyles.buttomStyle}
+            >
+              <Text style={Gstyles.buttomStyleTxt}>Cadastrar encomenda</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <TouchableOpacity onPress={() => sendForm()} style={styles.buttomPass}>
-          <Text style={styles.buttomPassTxt}>Cadastrar encomenda</Text>
-        </TouchableOpacity>
-      </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
