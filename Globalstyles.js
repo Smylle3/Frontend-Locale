abc68f79f450f0ae27ad607eb60b1f1d25a07a0d
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-03-08 21:23:01 -0300

    Correção de bugs e instalação do QRCode

--- Globalstyles.js
+++ Globalstyles.js
@@ -4,6 +4,31 @@ const Gstyles = StyleSheet.create({
   scroollContainer: {
     flex: 1,
     backgroundColor: "#fff0db",
+    
+  },
+  initTxt: {
+    margin: 10,
+    color: "#b2772c",
+  },
+  keyView: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  viewForm: {
+    alignItems: "center",
+    justifyContent: "center",
+
+    marginTop: 10,
+    padding: 5,
+    borderRadius: 8,
+    width: "80%",
+
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    elevation: 5,
   },
   buttomStyle: {
     justifyContent: "center",
